1a04e70eb26a5a71e23e83d668e94c3e60715c55
XuanqiLi <106274927+XuanqiLi@users.noreply.github.com>
2022-08-10 11:35:18 -0400

    Update 3in1 - please open in a separate folder.py

--- 3in1 - please open in a separate folder.py
+++ 3in1 - please open in a separate folder.py
@@ -30,45 +30,42 @@ def geturl(mb_html): #find url for each individual study's main page
     url_t = [urlfront + x for x in url]
     return url_t
 
-
-
-def getMetabolites_url(url_t):
-    for a in url_t:
-        mb_url_t = []
-        StudyPage = urllib.request.urlopen(a).read().decode('utf-8')
+def getMetaoblites(url_t):  #open the main page for study
+    for i in url_t:
+        StudyPage = urllib.request.urlopen(i).read().decode('utf-8')
+        print(i)
         mb_regex = re.compile('<a href=show_metabolites_by_study.php?.*>Show named metabolites</a>')
         mb_url = mb_regex.findall(StudyPage)
         mb_url = [i.lstrip("<a href=").split(">Show named metabolites</a>")[0] for i in mb_url]
         urlfront = 'https://www.metabolomicsworkbench.org/data/'
-        mb_url_0 = [urlfront + x for x in mb_url]
-        #x = open('test.txt', 'w')
-        #x.write('\n'.join(mb_url_t))
-        #x.close()
-        mb_url_t.append(mb_url_0)
-    return (mb_url_t)
+        mb_url_t = [urlfront + x for x in mb_url]
+        print(mb_url_t)
+        for i in mb_url_t:
+            url = i
+            studyIDpattern = re.compile('ST[0-9]+')
+            studyID = studyIDpattern.findall(url)
+            print(studyID)
+            metabolitespage = urllib.request.urlopen(i).read().decode('utf-8')
+            mb_regex = re.compile("MULTIPLE></td>[a-zA-Z0-9_()-±+',_\[\]α-ωΑ-Ω\s-]*?</td>",
+                                  re.DOTALL | re.M)  # this is the best I can came up with so far that can find all metabolites
+            mb_test = mb_regex.findall(metabolitespage)
+            mb_test = [a.replace('MULTIPLE></td><td>', '') for a in mb_test]
+            mb_test = [a.replace('</td>', '') for a in mb_test]
+            # mb_test = [i.lstrip('MULTIPLE></td><td>').split('</td>')[0] for i in mb_test]
+            filename = "%s.txt" % studyID
+            t = open(filename, 'w', encoding="utf-8")
+            t.write('\n'.join(mb_test))
+            t.close()
 
-#def getMetabolites(mb_url_t):
-#    for a in mb_url_t:
-#        url = a
-#        studyIDpattern = re.compile('ST[0-9]+')
-#        studyID = studyIDpattern.findall(url)
-#        #print(studyID)
-#        metabolitespage = urllib.request.urlopen(i).read().decode('utf-8')
-#        mb_regex = re.compile("MULTIPLE></td>[a-zA-Z0-9_()-±+',_\[\]α-ωΑ-Ω\s]*?</td>",re.DOTALL | re.M)  # this is the best I can came up with so far that can find all metabolites
-#        mb_test = mb_regex.findall(metabolitespage)
-#        mb_test = [i.lstrip('MULTIPLE></td><td>').split('</td>')[0] for i in mb_test]
-#        filename = "%s.txt" % studyID
-#        t = open(filename, 'w', encoding="utf-8")
-#        t.write('\n'.join(mb_test))
-#        return (mb_test)
+    return mb_test
+
 
 
 def main():
     mb_html = getGsmHtml()
     url_t = geturl(mb_html)
     downloadlink_t = getdownload(mb_html)
-    mb_url_t = getMetabolites_url(url_t)
-    #mb_test = getMetabolites(mb_url_t)
+    mb_test = getMetaoblites(url_t)
     g = open('study_url.txt', 'w')
     g.write('\n'.join(url_t))
     g.close()
@@ -77,10 +74,9 @@ def main():
     h.close()
     #print('3') #this is just to make sure this part of the code ran
     x = open('test.txt', 'w')
-    for i in mb_url_t:
-        x.write(i + '\n')
+    x.write('\n'.join(mb_url_t))
     x.close()
-    return(url_t,downloadlink_t,mb_url_t)#,mb_test)
+    return(url_t,downloadlink_t,mb_test)
 
 main()
 
