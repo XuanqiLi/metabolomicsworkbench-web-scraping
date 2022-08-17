614689f8d41af8cd2b3daef0e996c8af80cacb66
XuanqiLi <106274927+XuanqiLi@users.noreply.github.com>
2022-08-17 16:48:37 -0400

    Update Study_Details.py

--- Study_Details.py
+++ Study_Details.py
@@ -5,10 +5,37 @@ import os, sys
 import pandas as pd
 
 def getGsmHtml():
-    mb_url = 'https://www.metabolomicsworkbench.org/data/DRCCStudySummary.php?Mode=StudySummary&SortBy=Study%20ID&AscDesc=desc&ResultsPerPage=5000'
+    mb_url = 'https://www.metabolomicsworkbench.org/data/DRCCStudySummary.php?Mode=StudySummary&SortBy=Species&AscDesc=asc&ResultsPerPage=5000' #sort by species
+    #mb_url = 'https://www.metabolomicsworkbench.org/data/DRCCStudySummary.php?Mode=StudySummary&SortBy=Study%20ID&AscDesc=desc&ResultsPerPage=5000' #sort by studyID
     mb_html = urllib.request.urlopen(mb_url).read().decode('utf-8')
     #print('2') #this is just to make sure this part of the code ran
     return mb_html
+
+def getExcel():
+    df = pd.read_excel('Output.xlsx') #input file that need to be matched
+    studyID = df['Study ID'].tolist()
+    StudySummary = df['Study Summary'].tolist()
+    #print (StudySummary)
+    StudySummary = [str(x) for x in StudySummary]
+    StudySummary = [y.replace('-', ' ') for y in StudySummary]
+    StudySummary = [z.upper() for z in StudySummary]
+    StudySummary = [y.replace('(', ' ') for y in StudySummary]
+    StudySummary = [y.replace(')', ' ') for y in StudySummary]
+    #print(StudySummary)
+    return (studyID, StudySummary)
+
+
+def getExcel2():
+    df = pd.read_excel('word list.xlsx') #you can adjust word_list here
+    human_cell_line = df['Human Cell Line List'].tolist()
+    human_cell_line = [str(x) for x in human_cell_line]
+    human_cell_line = [y.replace('-',' ') for y in human_cell_line]
+    human_cell_line = [z.upper() for z in human_cell_line]
+    human_cell_line = [y.replace('(', ' ') for y in human_cell_line]
+    human_cell_line = [y.replace(')', ' ') for y in human_cell_line]
+    print (human_cell_line)
+    return (human_cell_line)
+
 
 def getStudyRow(mb_html):
     StudyRow_regex = re.compile('<tr class="odd"><td><[^<]+</a></td>\n<td>[^<]+</td>\n<td>[^<]+</td>\n<td>[^<]+</td>\n<td.*\n<td.*\n<td>[^<]+</td>\n<td>[0-9]+</td>|<tr class="even"><td><[^<]+</a></td>\n<td>[^<]+</td>\n<td>[^<]+</td>\n<td>[^<]+</td>\n<td.*\n<td.*\n<td>[^<]+</td>\n<td>[0-9]+</td>', re.DOTALL | re.M)
@@ -66,27 +93,35 @@ def getSampleSummary(url_t):
     return (sample_elements, summary_elements)
 
 
+def MatchCellLines(StudySummary, human_cell_line):
+    cell_line_list = []
+    #empty = ''
+    for i in StudySummary:
+        #print (i)
+        cell_line = ','.join(j for j in i.split() if j in human_cell_line)
+        cell_line_list.append(cell_line)
+    return (cell_line_list)
+
+
+
 def main():
     mb_html = getGsmHtml()
     StudyRow = getStudyRow(mb_html)
     url_t = geturl(mb_html)
     (sample_elements, summary_elements) = getSampleSummary(url_t)
     elements = getElements(StudyRow)
-#    print(len(studyID))
-#    print(i1)
-#    print(i2)
-#    print(pos1)
-#    print(pos2)
-#    print(String)
     print(elements)
     output =pd.DataFrame(elements, columns=['Study ID', 'Study Title', 'Species', 'Institute', 'Analysis', 'Released Date', 'Version', 'Samples'])
     output['Sample Type'] = pd.Series(sample_elements)
     output['Study Summary'] = pd.Series(summary_elements)
     output.to_excel('Output.xlsx')
+
+    (studyID, StudySummary) = getExcel()
+    (human_cell_line) = getExcel2()
+    cell_line_list = MatchCellLines(StudySummary, human_cell_line)
+    output['Cell Line Match'] = pd.Series(cell_line_list)
+    output.to_excel('Output.xlsx')
     print (3)
-    #t = open('test.txt', 'w', encoding="utf-8")
-    #t.write('\n'.join(StudyRow))
-    #t.close()
     return(StudyRow, url_t, sample_elements, summary_elements)
 
 
